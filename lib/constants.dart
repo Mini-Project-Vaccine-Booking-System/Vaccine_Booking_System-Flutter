38d368eba6d3313e2e5e36230d50934ca45b7dc6
apiiRR <ramadhanarafi437@gmail.com>
2022-06-03 17:55:55 +0700

    add constants color

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const kPrimary1 = Color(0xFF026DE1);
+const kPrimary2 = Color(0xFF66A7FF);
+
+const kPrimaryG2 = Color(0xFF238DFF);
+const kPrimaryG = Color(0xFF026DE1);
+
+const kMainBlack = Color(0xFF2D2D2D);
+const kMainWhite = Color(0xFFF6F6F6);
+
+const kNeutral1 = Color(0xFFB3B3B3);
+const kNeutral2 = Color(0xFFD4D4D4);
+const kNeutral3 = Color(0xFF8A8A8A);
+
+const kSuccess = Color(0xFF67F964);
+const kFail = Color(0xFFFF4F4F);
+const kFail2 = Color(0xFFDA7272);
