9814e19cda85c8f91be829a64493af65b935e2ae
apiiRR <ramadhanarafi437@gmail.com>
2022-06-09 18:20:55 +0700

    update vaccine pass  grouping widget

---
+++ lib/screens/pass_screen/pass_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:vaccine/screens/pass_screen/components/body.dart';
+
+import '../../constants.dart';
+
+class PassScreen extends StatelessWidget {
+  const PassScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Pendaftaran Berhasil"),
+          centerTitle: true,
+          backgroundColor: cPrimary1,
+          elevation: 0,
+        ),
+        body: Body(),
+    );
+  }
+}
